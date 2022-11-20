a2f11ac68395e4026dac6e59ff08bda6a69a6e0e
Aaron Morris <aaron.r.morris3@gmail.com>
2022-11-19 23:44:05 -0600

    passing arguments solved

---
+++ JS-Practice.js
@@ -0,0 +1,16 @@
+// We can pass values into a function with arguments. You can use a return statement to send a value back out of a function.
+//
+// Example
+
+function plusThree(num) {
+    return num + 3;
+}
+
+const answer = plusThree(5);
+
+// Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+function timesFive(int){
+    return int * 5;
+}
+// solved answer above *Freecode camp*
