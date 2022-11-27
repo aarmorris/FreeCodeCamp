4327ad2727260c3dc2722b3aa863ff1860c53b11
Aaron Morris <aaron.r.morris3@gmail.com>
2022-11-27 12:55:21 -0600

    addition of Golf Code

--- JS-Practice.js
+++ JS-Practice.js
@@ -646,3 +646,42 @@ orderMyLogic(7);
 // num < 20 - return Large
 // num >= 20 - return Huge
 
+function testSize(num) {
+    // Only change code below this line
+    if (num < 5) {
+        return "Tiny";
+    } else if (num < 10) {
+        return "Small";
+    } else if (num < 15) {
+        return "Medium";
+    } else if (num < 20) {
+        return "Large";
+    } else if (num >= 20) {
+        return "Huge";
+    }
+
+
+    return "Change Me";
+    // Only change code above this line
+}
+
+testSize(7);
+
+//Golf Code
+// In the game of Golf, each hole has a par, meaning, the average number of strokes a golfer is expected to make in order to sink the ball in the hole to complete the play. Depending on how far above or below par your strokes are, there is a different nickname.
+//
+// Your function will be passed par and strokes arguments. Return the correct string according to this table which lists the strokes in order of priority; top (highest) to bottom (lowest):
+//
+// Strokes	Return
+// 1	"Hole-in-one!"
+// <= par - 2	"Eagle"
+// par - 1	"Birdie"
+// par	"Par"
+// par + 1	"Bogey"
+// par + 2	"Double Bogey"
+// >= par + 3	"Go Home!"
+// par and strokes will always be numeric and positive. We have added an array of all the names for your convenience.
+
+
+
+
