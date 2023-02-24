ea0e2eed0e6a8f0a8a4d53a6140ec3b0c41c9ccf
Aaron Morris <aaron.r.morris3@gmail.com>
2023-02-24 12:27:07 -0600

    JS-Practice Functions Continued

--- JS-Practice.js
+++ JS-Practice.js
@@ -919,13 +919,13 @@ chainToSwitch(7);
 //
 // Sometimes people use an if/else statement to do a comparison, like this:
 //
-// function isEqual(a, b) {
-//   if (a === b) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+function isEqual(a, b) {
+  if (a === b) {
+    return true;
+  } else {
+    return false;
+  }
+}
 // But there's a better way to do this. Since === returns true or false, we can return the result of the comparison:
 //
 // function isEqual(a, b) {
