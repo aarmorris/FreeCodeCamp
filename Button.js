bf171cc30365e7715593681305eb563e20050f28
Aaron Morris <aaron.r.morris3@gmail.com>
2023-03-14 12:29:32 -0500

    Comment additional button

--- Button.js
+++ Button.js
@@ -1,8 +1,8 @@
 // $('.btn-click').on('click');
 
 
-$(document).ready(function () {
-    $(".btn-click").click(function () {
-        alert("This is an alert message!");
-    });
-});
+// $(document).ready(function () {
+//     $(".btn-click").click(function () {
+//         alert("This is an alert message!");
+//     });
+// });
